ac7ab4888331f3d1d1696b96ebdf6b87acab2b6b
Caroline Hwnag <pandabear.penguin.h@gmail.com>
2020-07-14 11:57:35 -0400

    Non-complete to-do list, KWK mini project

---
+++ ToDoList/AddToDoViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AddToDoViewController.swift
+//  ToDoList
+//
+//  Created by Caroline Hwang on 7/14/20.
+//  Copyright © 2020 Caroline Hwang. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+    
+    var previousVC = ToDoTableViewController()//making a reference to previous view controller so that we can use it
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = ToDo()
+        if let titleText = titleTextField.text {
+            toDo.name = titleText
+            toDo.important = importantSwitch.isOn
+        }
+        previousVC.toDos.append(toDo)
+        previousVC.tableView.reloadData()
+        navigationController?.popViewController(animated: true)
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Caroline Hwang on 7/13/20.
+//  Copyright © 2020 Caroline Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
